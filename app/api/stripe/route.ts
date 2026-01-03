029bf8510b9fdce8eaa7ceb945b1580833782b67
gemy3731 <mohamedgamalz3731@gmail.com>
2026-01-03 18:53:56 +0200

    feat(stripe): add cors

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -2,29 +2,41 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 const secretKey = process.env.STRIPE_SECRET_KEY;
 
-
 const stripe = new Stripe(secretKey!, {
   apiVersion: "2025-12-15.clover",
 });
 
 interface Product {
-    name: string;
-    description: string;
-    price: number;
-    quantity: number;
-  }
+  name: string;
+  description: string;
+  price: number;
+  quantity: number;
+}
+
+const allowedOrigin = process.env.NEXT_PUBLIC_ENV=== "production"?"https://ur-store-three.vercel.app":"http://localhost:5173";
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: {
+      "Access-Control-Allow-Origin": allowedOrigin,
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
 
 export async function POST(req: Request) {
-  const { host,products,cartId } = await req.json();
-    const selectedProducts: Product[] = products;
+  const { host, products, cartId } = await req.json();
+  const selectedProducts: Product[] = products;
   try {
     const session = await stripe.checkout.sessions.create({
       mode: "payment",
       payment_method_types: ["card"],
       payment_intent_data: {
-        metadata:{
-          cartId
-        }
+        metadata: {
+          cartId,
+        },
       },
       line_items: selectedProducts.map((product) => ({
         price_data: {
@@ -38,15 +50,19 @@ export async function POST(req: Request) {
         quantity: product.quantity,
       })),
       metadata: {
-        cartId
+        cartId,
       },
       success_url: `${host}/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${host}`,
     });
     console.log("Created Stripe session:", session);
-    return NextResponse.json({ url: session.url });
+    return NextResponse.json({ url: session.url },{
+      headers: {
+        "Access-Control-Allow-Origin": allowedOrigin,
+      },
+    });
   } catch (error) {
     console.error("Error fetching user:", error);
-    return NextResponse.json({ error: "User not found" }, { status: 404 });
+    return NextResponse.json({ error: "User not found" }, { status: 404,headers: {"Access-Control-Allow-Origin": allowedOrigin,}});
   }
 }
